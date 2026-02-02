6a28873cf140301abdd9a26e79b46841e53531ed
Prajwal H S <prajwalhs4india@gmail.com>
2026-02-02 15:09:03 +0530

    added gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import Footer from './../Components/Footer/Footer';
 import WhatsApp from './../Components/WhatsApp/WhatsApp';
 import Loader from './../Components/Loader/Loader';
 import { useEffect, useState } from "react";
+import Script from "next/script";
 
 
 
@@ -30,6 +31,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17917921555"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17917921555');
+            `,
+          }}
+        />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </head>
       <body >
